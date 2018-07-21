4b6466c3f1eb599d6481da882f524017a5a97d89
thuannguy <nguydthuan@hotmail.com>
2018-07-21 11:07:43 +0700

    Pushed sample project to demonstrate the System.Net.Http issue

---
+++ DemoLoadSystemNetHttp/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            System.Runtime.CompilerServices.Unsafe.Equals(null, null);
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://github.com");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
