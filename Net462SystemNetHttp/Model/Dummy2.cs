7f7f7dca5ba0d7187fb0854d020e0fc773e05bc1
thuannguy <nguydthuan@hotmail.com>
2018-07-28 16:25:01 +0700

    Inconsistent .NET 4.6.2 and System.Net.Http behavior

---
+++ Net462SystemNetHttp/Model/Dummy2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Model
+{
+    public class Dummy2
+    {
+        public static void DoNothing()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://github.com");
+        }
+    }
+}
---
+++ Net462SystemNetHttp/WebStuff/DummyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace WebStuff
+{
+    public static class DummyClass
+    {
+        public static void DoNothing()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://github.com");
+        }
+    }
+}
