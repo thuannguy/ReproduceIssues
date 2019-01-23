1da9756660757cc404a3512b7cb5cecd4c8721db
thuannguy <thuannguy@users.noreply.github.com>
2019-01-23 16:44:44 +0700

    Pushed more sample test

---
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Note: add System.IdentityModel.LocalAppContextSwitches.DisableCngCertificates to the watch window to debug
+            //AppContext.SetSwitch("TestSwitch.LocalAppContext.DisableCaching", true);
+            AppContext.TryGetSwitch("Switch.System.ServiceModel.DisableCngCertificates", out bool isEnabled);
+            //AppContext.SetSwitch("Switch.System.ServiceModel.DisableCngCertificates", true);
+            AppContext.TryGetSwitch("Switch.System.ServiceModel.DisableCngCertificates", out isEnabled);
+            System.IdentityModel.CookieTransform ct = new System.IdentityModel.DeflateCookieTransform();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
